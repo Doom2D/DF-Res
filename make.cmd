584ee22724e2ea325e0fee3bee8efa5165a62f4f
binarymaster <x86corez@gmail.com>
2016-01-03 11:07:49 +1000

    Add make.cmd for quick build, improve pack_dnd.cmd

---
+++ make.cmd
@@ -0,0 +1,10 @@
+@echo off
+cd /d %~dp0
+call pack_dnd Game.lst
+call pack_dnd Editor.lst
+call pack_dnd Standart.lst
+call pack_dnd Doomer.lst
+call pack_dnd Doom2D.lst
+echo [*] Build All done.
+echo.
+pause
--- pack_dnd.cmd
+++ pack_dnd.cmd
@@ -1,4 +1,8 @@
 @echo off
 cd /d %~dp0
-if exist %1 dfwad.exe %1 %~n1.wad -i
-pause
+if exist %1 (
+echo [*] Building %~n1.wad ...
+echo.
+dfwad.exe %1 %~n1.wad -i
+echo.
+)

47dcf70a9fa94cb6d3d6c60875856b896ec6eb9f
Dmitry D. Chernov <blackdoomer@yandex.ru>
2017-10-02 15:54:02 +1000

    Add ShrShade.wad resources (0.666)

--- make.cmd
+++ make.cmd
@@ -3,6 +3,7 @@ cd /d %~dp0
 call pack_dnd Game.lst
 call pack_dnd Editor.lst
 call pack_dnd Standart.lst
+call pack_dnd ShrShade.lst
 call pack_dnd Doomer.lst
 call pack_dnd Doom2D.lst
 echo [*] Build All done.

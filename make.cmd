5abf3472faff9de5fb75bd624673be05b592b102
Stas'M <x86corez@gmail.com>
2018-10-02 15:29:17 +0300

    DFWAD: Return correct error code

--- make.cmd
+++ make.cmd
@@ -1,10 +1,10 @@
 @echo off
 cd /d %~dp0
-call pack_dnd Game.lst
-call pack_dnd Editor.lst
-call pack_dnd Standart.lst
-call pack_dnd ShrShade.lst
-call pack_dnd Doomer.lst
-call pack_dnd Doom2D.lst
+for %%i in (*.lst) do (
+  call pack_dnd %%i
+  IF ERRORLEVEL 1 (
+    exit /b
+  )
+)
 echo [*] Build All done.
 echo.

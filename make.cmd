ed4556c41f4de17a985b0b583f52a7df14743cc8
Stas'M <x86corez@gmail.com>
2018-10-02 20:05:09 +0300

    Standart: Get rid of precompiled DFWADs

--- make.cmd
+++ make.cmd
@@ -1,10 +1,29 @@
 @echo off
 cd /d %~dp0
+set PATH=%PATH%;%CD%
+call :treeProcess
+goto :done
+
+:treeProcess
+for /D %%d in (*) do (
+    cd %%d
+    call :treeProcess
+    cd ..
+)
 for %%i in (*.lst) do (
-  call pack_dnd %%i
+  if exist %%i (
+    echo [*] Building %%~ni.wad ...
+    echo.
+    dfwad.exe %%i %%~ni.wad -i
+    echo.
+  )
   IF ERRORLEVEL 1 (
     exit /b
   )
 )
-echo [*] Build All done.
-echo.
+exit /b
+:done
+IF NOT ERRORLEVEL 1 (
+  echo [*] Build All done.
+  echo.
+)
